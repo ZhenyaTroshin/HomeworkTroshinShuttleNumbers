5c8624af0679e09fb8f3f176c7c2638d80ccde73
ZhenyaTroshin <200troshin@gmail.com>
2023-08-15 03:14:25 +0300

    Number Shuttles

--- Src/Shuttle.java
+++ Src/Shuttle.java
@@ -1,20 +1,26 @@
 public class Shuttle {
+
     public static void main(String[] args) {
+
         int shuttls = 1;
         for (int i = 0; i < 100; i++) {
             System.out.println(shuttls);
-            shuttls++;
+                shuttls++;
+
             if ((shuttls - 4) % 10 == 0) {
                 shuttls++;
             }
+
             if ((shuttls - 9) % 10 == 0) {
-                shuttls++;
+                 shuttls++;
             }
+
             if ((shuttls - 100 * (shuttls / 100)) / 10 == 4) {
-                shuttls += 10;
+                 shuttls += 10;
             }
+
             if ((shuttls - 100 * (shuttls / 100)) / 10 == 9) {
-                shuttls += 10;
+                 shuttls += 10;
             }
         }
     }
